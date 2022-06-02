c7a756a6395c982e9b21316208af755b484c267c
Fitsumtesfa <fitsumtesfa77@gmail.com>
2022-06-02 12:41:04 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,19 @@
-#include <stdio.h>
-
 /*
- *  main - Entry point
- *   
- *    Description: prints "with proper grammar, but the outcome is a piece of
- *     art,"
- *
- *      Return: Always 0 (Success)
- *       */
+ *   main - print the size of various types
+ *    
+ *      Description: prints the size of various types on the computer it is compiled
+ *        and run on
+ *         
+ *           Return: Allways 0 (Success)
+ *            */
+
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+		printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
+			printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
+				printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
+					printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
+						printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
+
+							return (0);
 }
