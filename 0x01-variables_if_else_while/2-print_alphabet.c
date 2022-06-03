c08dd650b9c1c05bbb08e2c6580c6135dd8bc24a
fitsumtesfa <105015691+fitsumtesfa@users.noreply.github.com>
2022-06-03 21:42:52 +0300

    Update 2-print_alphabet.c
     a program that prints the alphabet in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,23 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - print the letters of the alphabet
+ *
+ * Description: print the letters of the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
